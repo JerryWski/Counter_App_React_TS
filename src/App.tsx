b64079d1a6b957ac9de319c1b2443a2380510608
JerryWski <jarek_wasilewski@yahoo.com>
2023-06-19 21:32:04 +0200

    setting up counter app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import styles from './App.module.css';
+
+const App: React.FC = () => {
+  return <div className={styles.main_app}>Counter</div>;
+};
+
+export default App;
