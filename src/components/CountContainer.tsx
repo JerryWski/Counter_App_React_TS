1a97d36cc571cb5b26bfe70387f61d578bbde6c4
JerryWski <jarek_wasilewski@yahoo.com>
2023-06-20 19:16:02 +0200

    styling inputs container

--- src/components/CountContainer.tsx
+++ src/components/CountContainer.tsx
@@ -1,9 +1,11 @@
 import styles from './CountContainer.module.css';
+import CountInputsWrapper from './CountInputsWrapper';
 
 const CountContainer = () => {
   return (
     <div className={styles.bg_wrapper}>
       <h2 className={styles.count_header}>Let&apos;s Count It!</h2>
+      <CountInputsWrapper />
     </div>
   );
 };
--- src/components/CountInputsWrapper.tsx
+++ src/components/CountInputsWrapper.tsx
@@ -1,7 +1,28 @@
+import styles from './CountInputsWrapper.module.css';
+
 const CountInputsWrapper = () => {
   return (
-    <div>
-      <div>Tu beda inputy</div>
+    <div className={styles.inputs_wrapper}>
+      <span className={styles.error_text__amount}>Error text amount</span>
+      <div className={styles.input_container}>
+        <input required type="number" />
+        <span>Starting amount</span>
+        <i></i>
+      </div>
+      <div className={styles.input_container}>
+        <input required type="number" />
+        <span>Annunal rate(%)</span>
+        <i></i>
+      </div>
+      <div className={styles.input_container}>
+        <input required type="number" />
+        <span>Duration*</span>
+        <i></i>
+      </div>
+      <span className={styles.error_text}>error text</span>
+      <p className={styles.duration_explain}>
+        * In duration please select numbers from 1 to 12
+      </p>
     </div>
   );
 };
