4f6f9663a89440cf8b31145d7adf571270be50c2
JerryWski <jarek_wasilewski@yahoo.com>
2023-07-08 21:15:18 +0200

    adding color scheme for light/dark mode swith and some fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,16 +8,19 @@ const Header = () => {
         Do you know what is the difference between an income for a bank deposit
         and an investment fund?
       </h2>
-      <h2 className={styles.text}>
+      <p className={styles.text}>
         In my past business of finance, I havee noticed that many people can not
         accurately calculate or compare the return on a term deposit and a
         mutual fund, where we have the same parameters, namely the amount, the
-        rate of return and the duration. Therefore, I combined my previous
-        experience in the financial industry along with my acquired skills in
-        programming and decided to create this simple application/tool for you.
-        If you want to quickly count the difference in profit, you will do it
-        with this tool, try it and count it!
-      </h2>
+        rate of return and the duration.
+      </p>
+      <p className={styles.text}>
+        Therefore, I combined my previous experience in the financial industry
+        along with my acquired skills in programming and decided to create this
+        simple application/tool for you. If you want to quickly count the
+        difference in profit, you will do it with this tool, try it and count
+        it!
+      </p>
     </div>
   );
 };
